60bd304cba3af4464f095bd722e20b637cd6aecc
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-21 22:43:44 +0600

    added json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import navImg from './assets/logo.png';
+import dollarImg from './assets/Currency.png';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar max-w-7xl mx-auto">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="text-xl">
+            <img className='w-[60px] h-[60px]' src={navImg} alt="" />
+          </a>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-          </button>
+        <div className="flex items-center">
+          <span className='mr-1'>6000000</span>
+          <span className='mr-1'> Coins</span>
+          <img src={dollarImg} alt="" />
         </div>
       </div>
     </>
