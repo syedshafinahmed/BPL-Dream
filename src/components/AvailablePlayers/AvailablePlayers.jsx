6bb4163ed2ad063dce6ade2d98a520d093dedf05
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-22 02:49:40 +0600

    displayed json data

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -6,7 +6,7 @@ const AvailablePlayers = ({ playersPromise }) => {
     const playerData = use(playersPromise)
     // const {image, name, country, role, rating, batting_style, bowling_style, price_usd} = playerData;
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className='max-w-7xl mx-auto grid grid-cols-3 justify-items-center gap-y-8'>
 
         {
             playerData.map(player => <div className="card bg-base-100 w-96 shadow-sm">
@@ -17,20 +17,20 @@ const AvailablePlayers = ({ playersPromise }) => {
                 </figure>
                 <div className="p-3">
                     <div className='flex gap-x-3 items-center'>
-                        <img className='w-[20px] h-[22px]' src={userImg} alt="" /> <h2 className="card-title">{player.name}</h2>
+                        <img className='w-[20px] h-[22px]' src={userImg} alt="" /> <h1 className="font-black text-2xl">{player.name}</h1>
                     </div>
                     <div className='flex justify-between items-center mt-3 border-b-1 border-gray-600 pb-3'>
                         <div className='flex gap-x-3 items-center'>
-                            <img className='w-[15px] h-[15px]' src={flagImg} alt="" /> <h2 className="card-title">{player.country}</h2>
+                            <img className='w-[15px] h-[15px]' src={flagImg} alt="" /> <h4 className="card-title">{player.country}</h4>
                         </div>
                         <p>{player.role}</p>
                     </div>
                     <p className='mt-3'>Rating: {player.rating}</p>
-                    <div className='flex justify-between '>
+                    <div className='flex justify-between mt-4 mb-3'>
                         <p>{player.batting_style}</p>
                         <p>{player.bowling_style}</p>
                     </div>
-                    <div className="flex justify-between mt-3 items-center">
+                    <div className="flex justify-between items-center">
                         <p>Price: $ {player.price_usd}</p>
                         <button className="btn">Choose Player</button>
                     </div>
